806d68b0110603ff0d37e285d9ba8d87263f97fa
Alan Santos <alan.sslva@outlook.com>
2017-12-11 15:07:09 -0200

    test

--- index.php
+++ index.php
@@ -14,24 +14,49 @@ $port           = $_GET['source-port'];
 
 $ve = new hbattat\VerifyEmail($email, $source_email, $port);
 
-echo $ve->verify();
+
+$domain = $ve->return_domain($email);
+if(in_array(strtolower($domain),['yahoo.com'])) {
+
+    if ($ve->verify()){
+
+        echo '<pre>';
+        print_r($ve->get_errors());
+        echo '</pre>';
+
+        echo '<pre>';
+        print_r($ve->get_debug());
+        echo '</pre>';
 
 
-echo '<pre>';
-print_r($ve->get_errors());
-echo '</pre>';
+        if(array_key_exists('EMAIL_FOUNDED', $ve->get_debug())){
+            if($ve->get_debug()['EMAIL_FOUNDED'] == true){
+                echo 1;
+            }
+        }else{
+            echo 0;
+        }
 
-echo '<pre>';
-print_r($ve->get_debug());
-echo '</pre>';
+    }else{
 
+        echo 'invalid address';
 
-if(array_key_exists('EMAIL_FOUNDED', $ve->get_debug())){
-   if($ve->get_debug()['EMAIL_FOUNDED'] == true){
-       echo 1;
-   }
+    }
+
 }else{
-    echo 0;
+
+    echo '<pre>';
+    print_r($ve->get_errors());
+    echo '</pre>';
+
+    echo '<pre>';
+    print_r($ve->get_debug());
+    echo '</pre>';
+
+
 }
+
+
+
 
 
